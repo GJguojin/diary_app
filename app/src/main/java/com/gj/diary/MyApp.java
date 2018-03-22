1abc6e413b0ad692386d8537a089dff1e5dfb8d2
jin.guo 郭晋 <jin.guo@qiyuesuo.com>
2018-03-22 15:55:35 +0800

    1.1版本字体

--- app/src/main/java/com/gj/diary/MyApp.java
+++ app/src/main/java/com/gj/diary/MyApp.java
@@ -2,6 +2,9 @@ package com.gj.diary;
 
 import android.app.Application;
 import android.content.Context;
+import android.graphics.Typeface;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by zhufeng on 2017/1/10.
@@ -15,6 +18,17 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
         mContext = getApplicationContext();
+
+        final Typeface fromAsset = Typeface.createFromAsset(getAssets(), "fonts/shouxie.ttf");
+        try {
+            Field field = Typeface.class.getDeclaredField("MONOSPACE");
+            field.setAccessible(true);
+            field.set(null, fromAsset);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Context getAppContext(){
